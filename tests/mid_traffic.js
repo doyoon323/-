6e5a09870523a7134137bb1ed2580dcc5a2eba8d
dodo_kdy <dyn323@naver.com>
2026-01-16 03:03:43 +0900

    feat : 낙관적 락 구현, WAS 3대 확장

--- tests/mid_traffic.js
+++ tests/mid_traffic.js
@@ -3,7 +3,7 @@ import { check, sleep } from 'k6';
 
 export const options = {
   vus: 100,
-  iterations: 200,
+  iterations: 1000,
 };
 
 // 1. 실제 테스트용 토큰과 유저 ID를 여기에 넣으세요
@@ -11,7 +11,7 @@ const TEST_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzMiIsImlhdCI6MTc2NzY4MDk4Miw
 const TEST_USER_ID = 32; 
 
 export default function () {
-  const url = 'http://localhost:8080/api/reservations'; // 주소 수정
+  const url = 'http://localhost:8080/api/reservations?type=atomic'; // 주소 수정
   
   const payload = JSON.stringify({
     userId: TEST_USER_ID,    // 필드명 수정
